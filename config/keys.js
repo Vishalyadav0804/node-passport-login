f303aaf82917e888e2323fd0313bbb83b50cee92
Vishal Yadav <vishalyadav0804@gmail.com>
2020-03-30 08:08:56 +0530

    Commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+//dbPassword = 'mongodb+srv://Vishalyadav0804:Vishal@0804@test-cluster-kkm8h.mongodb.net/test?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: 'mongodb+srv://Vishalyadav0804:Vishal@0804@test-cluster-kkm8h.mongodb.net/test?retryWrites=true&w=majority'
+};
